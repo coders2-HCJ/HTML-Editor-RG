5dc429921e292a1eb37130618af3c8ce9ed676a7
kitteneditorc0der <115348427+kitteneditorc0der@users.noreply.github.com>
2022-10-14 14:14:54 +0800

    Create editor.js

---
+++ editor.js
@@ -0,0 +1,27 @@
+const first = document.querySelector(".first");
+const iframe = document.querySelector("iframe");
+const elem = document.documentElement;
+
+var fsc = function() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        elem.msRequestFullscreen();
+    }
+};
+
+var html = first.textContent;
+iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+
+first.addEventListener("keyup", function() {
+    var html = first.textContent;
+    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+});
+
+first.addEventListener("paste", function(event) {
+    event.preventDefault();
+    var text = event.clipboardData.getData("text/plain");
+    document.execCommand("insertText", false, text);
+});
